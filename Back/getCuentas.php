5a893aaabbb072d0e2d22c237ac829f0e4a38908
daniel <danielmonr@gmail.com>
2016-12-05 15:28:29 -0600

    Algo

--- Back/getCuentas.php
+++ Back/getCuentas.php
@@ -2,17 +2,17 @@
    include_once("DBConnection.php");
    include_once("session.php");
    $db = new DBConnection();
-   echo "hola"
+   echo "hola";
    $dbc = $db->Connect();
    $sql = "select * from cuenta where id_usuario=" . $_SESSION['login_user'];
    if ($result = $dbc->query($sql)){
 	   // output data of each row
-	   $json = '{"items":['
+	   $json = '{"items":[';
 	   while($r = $result->fetch_assoc()){
 		   $json = $json . json_encode($r);
 	   }
 
-	   $json = $json . ']}'
+	   $json = $json . ']}';
      echo $json;
    } else {
      echo 0;
