8a94218e3cef59e0d9ad47d06a225f958da2def2
daniel <danielmonr@gmail.com>
2016-12-05 15:38:27 -0600

    Algo

--- Back/getCuentas.php
+++ Back/getCuentas.php
@@ -1,6 +1,6 @@
 <?php
    include_once("DBConnection.php");
-   include_once("session.php");
+   //include_once("session.php");
    $db = new DBConnection();
    echo "hola";
    $dbc = $db->Connect();
