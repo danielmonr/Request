84fb4074419b381bcf6d04c6b446b9cf83ab3f7d
daniel <danielmonr@gmail.com>
2016-12-05 15:52:28 -0600

    Algo

--- Back/session.php
+++ Back/session.php
@@ -1,10 +1,7 @@
 <?php
-    include_once('DBConnection.php');
-    $db = new DBConnection();
-    $dbc = $db->Connect();
     session_start();
     //print("Se está corriendo<br/>");
-    if(!isset($_SESSION['login_user'])){
+    if(!isset($_SESSION['user_type'])){
         header("location: http://ubiquitous.csf.itesm.mx/~daw-1021514/content/Request/FRONT/login.html");
-    }
+	}
 ?>
