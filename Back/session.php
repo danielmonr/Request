821f49e7647d0fec0a56d4340bc7fe4db0888c27
daniel <danielmonr@gmail.com>
2016-12-05 15:32:43 -0600

    Algo

--- Back/session.php
+++ Back/session.php
@@ -5,7 +5,7 @@
     session_start();
     //print("Se está corriendo<br/>");
     if(!isset($_SESSION['login_user'])){
-        header("location: http://ubiquitous.csf.itesm.mx/~daw-1021514/content/Request/Front/login.html");
+        header("location: http://ubiquitous.csf.itesm.mx/~daw-1021514/content/Request/FRONT/login.html");
     }
     $user_id = $_SESSION['login_user'];
     $sql = mysqli_query($dbc,"SELECT nombre from exf_Clientes WHERE id = '$user_id' ");
